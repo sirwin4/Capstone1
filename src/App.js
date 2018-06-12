324fa4cb32335e8e501564130e2031c5686a67ca
sirwin4 <sean.irwinxc@gmail.com>
2018-06-12 11:55:16 -0500

    search list working

--- src/App.js
+++ src/App.js
@@ -4,38 +4,92 @@ import SearchResult from './searchResult'
 
 
 class App extends Component {
-  state = {
-    movie: "",
-    overview: "",
-    release: ""
-  }
+  constructor(props){
+    super(props)
   
-  getMovie =  () => {
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=d1828a2d110346eee0cfdd42f858ba1e&language=en-US&query=Red&page=1&include_adult=false`) //${SearchResult}
+  this.state = {
+    term: "",
+    movie: [],
+    overview: [],
+    release: [],
+    element: []
+  }}
+
+  handleChange = function (event) {
+    this.setState({term: event.target.value})
+  }.bind(this)
+  
+  getMovie = function (event) {
+    event.preventDefault()
+    this.setState({
+      movie: [],
+      overview: [],
+      release: [],
+      element: [] 
+    })
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=d1828a2d110346eee0cfdd42f858ba1e&language=en-US&query=${encodeURI(this.state.term)}&page=1&include_adult=false`) 
     .then(result => result.json())
     .then(result => {
-      this.setState({
-        movie: result.results[0].original_title,
-        overview: result.results[0].overview,
-        release: result.results[0].release_date
+      if (result.total_results !== 0){
+        let movieArray =[]
+        let overviewArray =[]
+        let releaseArray =[]
+        for (let index = 0; index < 5; index++) {
+          if(index <= result.total_results){
+            movieArray.push(result.results[index].original_title)
+            overviewArray.push(result.results[index].overview)
+            releaseArray.push(result.results[index].release_date)
+          }
+        }
+        this.setState({
+          movie: movieArray,
+          overview: overviewArray,
+          release: releaseArray
+        })
+      }
+      else {
+        this.setState({
+          movie: [""],
+          overview: ["Please Revise Search"],
+          release: [""]
+        }
+        )
+      }
+    }
+    )
+    .then(result => {
+      if(this.state.movie.length === this.state.overview.length && this.state.overview.length === this.state.release.length){
+      for (let index = 0; index < this.state.movie.length; index++) {
+        this.state.element.push(
+          <div>
+            <h2>{this.state.movie[index]}</h2>
+            <p>{this.state.overview[index]}</p>
+            <p>{this.state.release[index]}</p>
+          </div>
+        )
+      }
+      this.forceUpdate()
+    }
+  })
+  }.bind(this)
+  
 
-      })
-    })
-}
-
-componentDidMount () {
-  this.getMovie()
-}
 
   render() {
     return (
       <div className="App">
-        <h2>{this.state.movie}</h2>
-        <p>{this.state.overview}</p>
-        <p>{this.state.release}</p>
+        <form onSubmit={this.getMovie}>
+        <input
+          type="text"
+          placeholder="Enter your search term here"
+          onChange={this.handleChange}
+        />
+        <button type="Submit">Submit</button>
+        </form>
+        <div>{this.state.element[0]}{this.state.element[1]}{this.state.element[2]}{this.state.element[3]}{this.state.element[4]}</div>
       </div>
     );
-}
+  }
 }
 
 export default App;
